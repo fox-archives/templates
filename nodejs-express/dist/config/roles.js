8fd4de0cd0aa828158567a28879ccfd2c84d1ee4
Edwin Kofler <edwin@kofler.dev>
2023-03-08 18:40:39 -0800

    initial commit

---
+++ nodejs-express/dist/config/roles.js
@@ -0,0 +1,6 @@
+const allRoles = {
+    user: [],
+    admin: ['getUsers', 'manageUsers'],
+};
+export const roles = Object.keys(allRoles);
+export const roleRights = new Map(Object.entries(allRoles));
---
+++ nodejs-express/dist/routes/v1/docs.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = express.Router();
+// const specs = swaggerJsdoc({
+//   swaggerDefinition,
+//   apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+// });
+// router.use('/', swaggerUi.serve);
+// router.get(
+//   '/',
+//   swaggerUi.setup(specs, {
+//     explorer: true,
+//   })
+// );
+export default router;
